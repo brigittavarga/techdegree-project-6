4a2435b942d722877485dba13e8796bbdf2892f4
Brigitta Varga <v.brigitta.zs@gmail.com>
2020-12-01 10:48:38 +0100

    adding code

---
+++ js/app.js
@@ -0,0 +1,10 @@
+const qwerty = document.getElementById("qwerty");
+const phrase = document.getElementById("phrase");
+const buttonReset = document.getElementsByClassName("btn__reset");
+
+// Keep track of the number of guesses the player has missed 
+const missed = 0;
+
+const phrases = arr => {
+    
+}
